8f6a6952461b01110830777b841a0755b0264b6e
harshal-ishere <harshal011kamble@gmail.com>
2024-03-22 14:53:53 +0530

    something

---
+++ ui/.svelte-kit/generated/client/app.js
@@ -0,0 +1,27 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [4],
+		"/login": [5,[2]],
+		"/register": [6,[3]]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
