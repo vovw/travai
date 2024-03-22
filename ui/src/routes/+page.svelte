227735aeff4a4bc73a2a6f0d5a19a047a37437e7
atharva <voidz7447@gmail.com>
2024-03-23 04:58:31 +0530

    added betteer frontend

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<main>
+    <div class="com hero min-h-screen">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-8xl font-bold">TripAI</h1>
+                <p class="py-10 text-4xl">
+                    Spend less time planning and more on enjoying trips.
+                </p>
+                <a href="/explore">
+                    <button class="btn btn-primary">Plan your next trip!</button
+                    >
+                </a>
+            </div>
+        </div>
+    </div>
+</main>
