c5485bc24384ab787bbf5fb4490a80dcd1c1a7e7
devXyz <ranesoham301@gmail.com>
2024-03-22 19:02:18 +0530

    Finally scrapping complete

--- server/scraper/temp.js
+++ server/scraper/temp.js
@@ -1,32 +1,47 @@
 import fs from 'fs';
 import { getHotelsdata } from "./hotels.js";
 import { getMMTFlightData } from "./makeMyTripScrape.js";
-import { getDateFormatted } from "./commons.js";
+import { getDateFormatted,convertDateFormat } from "./commons.js";
 import { getYatraFlightData } from "./yatraScrape.js";
 import { clearPastData } from "./clearPastData.js";
 
-const [citycode, checkinDate, checkoutDate] = ["CTBOM", "03272024", "03282024"]
-const placeFrom = 'New Delhi';
-const placeTo = 'Pune';
+const outDate = "29 March 2024"
 const date = '28 March 2024';
+const [citycode, checkinDate, checkoutDate] = ["CTBOM",convertDateFormat(date),convertDateFormat(outDate)]
+const placeFrom = 'Mumbai';
+const placeTo = 'Pune';
 const [ dateDay,dateMonth,dateYear ] = date.split(' ');
+const [ outDateDay,outDateMonth,outDateYear ] = outDate.split(' ');
 
-getHotelsdata('BOM', checkinDate, checkoutDate)
+export const scrape = async () => {
+    const result = {
+        "source" : placeFrom,
+        "destination" : placeTo,
+        "DateStart" : getDateFormatted(date),
+        "DateEnd" : convertDateFormat(outDate),
+        "hotels" : [],
+        "depFlights" : [],
+        "retFlights" : []
+    };
+    await clearPastData()
+    await getHotelsdata(citycode, checkinDate, checkoutDate)
+    await getMMTFlightData(placeFrom, placeTo, dateDay, dateMonth, dateYear)
+    // await getYatraFlightData(placeFrom, placeTo, getDateFormatted(date))
 
-// export const scrape = async () => {
-//     const result = {
-//         "source" : placeFrom,
-//         "destination" : placeTo,
-//         "DateStart" : getDateFormatted(date),
-//         "DateEnd" : "10/04/2024",
-//         "hotels" : [],
-//         "depFlights" : [],
-//         "retFlights" : []
-//     };
-//     await clearPastData()
-//     await getHotelsdata(citycode, checkinDate, checkoutDate)
-//     await getMMTFlightData(placeFrom, placeTo, dateDay, dateMonth, dateYear)
-//     await getYatraFlightData(placeFrom, placeTo, getDateFormatted(date))
+    const hotels = fs.readFileSync('hotels.json');
+    const flights = fs.readFileSync('flights.json');
+    result.hotels = await JSON.parse(hotels);
+    result.depFlights = await JSON.parse(flights);
+    await clearPastData()
 
-// }
+    await getMMTFlightData(placeTo,placeFrom, outDateDay, outDateMonth, outDateYear)
+    const flights2 = fs.readFileSync('flights.json');
+    result.retFlights = await JSON.parse(flights2);
+
+    console.log(result);
+    fs.writeFileSync('result.json', JSON.stringify(result));
+    return result;
+}
+
+scrape()
 
