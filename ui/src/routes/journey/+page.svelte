5ae31e863895f289dae4a6542f5c0737f2b4a8d8
Abhay Upadhyay <urabhay10@gmail.com>
2024-03-23 13:35:40 +0530

    calender fixes

--- ui/src/routes/journey/+page.svelte
+++ ui/src/routes/journey/+page.svelte
@@ -7,7 +7,7 @@
     if (typeof window !== "undefined") {
         placeTo = JSON.parse(localStorage.getItem("answers") || "").placeTo;
     }
-    let chatDetail = '';
+    let chatDetail = "";
     let eventname = "";
     let currval = "";
     let cards = [
@@ -74,26 +74,26 @@
             }
         }
     }
-    async function getChat(){
-        const response = await fetch("http://localhost:4000/llm/about-place", {
+    async function getChat() {
+        const response = await fetch("http://localhost:4000/llm/get-details", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 Authorization: Cookies.get("user"),
             },
             body: JSON.stringify({
-                place: placeTo,
+                dest: placeTo,
             }),
         });
         const data = await response.json();
-        typeChat(data.about);
+        typeChat(data);
     }
     async function typeChat(text: any) {
         let i = 0;
         if (i < text.length) {
             chatDetail += text[i];
             i++;
-            setTimeout(typeChat, 30, text.substring(i));
+            setTimeout(typeChat, 10, text.substring(i));
         }
     }
     // console.log(dates);
@@ -102,10 +102,10 @@
         dates.map(() => {
             i++;
         });
-        console.log(dates)
-        console.log(i)
-        console.log(Math.floor(parseInt(arrivalFlight.starttime.hours) / 6))
-        console.log(Math.floor(parseInt(departureFlight.starttime.hours) / 6))
+        console.log(dates);
+        console.log(i);
+        console.log(Math.floor(parseInt(arrivalFlight.starttime.hours) / 6));
+        console.log(Math.floor(parseInt(departureFlight.starttime.hours) / 6));
         return (
             Number(i * 200) -
             Math.floor(parseInt(arrivalFlight.starttime.hours) / 6) * 50 -
@@ -117,7 +117,7 @@
     $: calenderHeight = getcalendarheight();
     let suggestions: any[] = [];
     onMount(async () => {
-        //getChat();
+        getChat();
         const result = await fetch("http://localhost:4000/trip/get-results", {
             method: "POST",
             headers: {
@@ -160,16 +160,16 @@
         };
         departureFlight = {
             startdate:
-            data.retFlight.flightDate.split("/")[0] +
-            " " +
-            numtoMonth(parseInt(data.retFlight.flightDate.split("/")[1])),
+                data.retFlight.flightDate.split("/")[0] +
+                " " +
+                numtoMonth(parseInt(data.retFlight.flightDate.split("/")[1])),
             starttime: {
                 hours: data.retFlight.flightTime.split(":")[0],
                 minutes: data.retFlight.flightTime.split(":")[1],
             },
             duration: 1,
         };
-        console.log(arrivalFlight,departureFlight)
+        console.log(arrivalFlight, departureFlight);
 
         const llmData = await fetch("http://localhost:4000/llm/do-magic", {
             method: "POST",
@@ -182,45 +182,45 @@
             }),
         });
         const llm = await llmData.json();
-        suggestions = llm.places;
+        // suggestions = llm.places;
         calenderHeight = getcalendarheight();
-        // suggestions = {
-            //     places: [
-        //         {
-        //             placeName: "Sanjay Gandhi National Park",
-        //             startTime: {
-        //                 hours: "10",
-        //                 minutes: "00",
-        //             },
-        //             duration: {
-        //                 hours: "3",
-        //                 minutes: "00",
-        //             },
-        //         },
-        //         {
-        //             placeName: "Elephanta Caves",
-        //             startTime: {
-        //                 hours: "13",
-        //                 minutes: "00",
-        //             },
-        //             duration: {
-        //                 hours: "2",
-        //                 minutes: "30",
-        //             },
-        //         },
-        //         {
-        //             placeName: "Gateway of India",
-        //             startTime: {
-        //                 hours: "16",
-        //                 minutes: "00",
-        //             },
-        //             duration: {
-        //                 hours: "1",
-        //                 minutes: "30",
-        //             },
-        //         },
-        //     ],
-        // }.places;
+        suggestions = {
+            places: [
+                {
+                    placeName: "Sanjay Gandhi National Park",
+                    startTime: {
+                        hours: "10",
+                        minutes: "00",
+                    },
+                    duration: {
+                        hours: "3",
+                        minutes: "00",
+                    },
+                },
+                {
+                    placeName: "Elephanta Caves",
+                    startTime: {
+                        hours: "13",
+                        minutes: "00",
+                    },
+                    duration: {
+                        hours: "2",
+                        minutes: "30",
+                    },
+                },
+                {
+                    placeName: "Gateway of India",
+                    startTime: {
+                        hours: "16",
+                        minutes: "00",
+                    },
+                    duration: {
+                        hours: "1",
+                        minutes: "30",
+                    },
+                },
+            ],
+        }.places;
         let temp = data.places.map((e: any) => {
             return e.placeName;
         });
@@ -325,7 +325,7 @@
                 class="card card-body items-center text-center date-div bg-base-200 rounded"
                 style="height: 196px;border:1px solid white;top:10px;"
             >
-                <h2 class="card-title font-bold text-base-content">{date}</h2>  
+                <h2 class="card-title font-bold text-base-content">{date}</h2>
             </div>
         {/each}
     </div>
@@ -340,14 +340,22 @@
         <div class="h-full border-r border-gray-300 mx-4"></div>
     </div> -->
     <div class="mx-10 suggestions">
+        <div class="card card-body items-center text-center date-div">
+            <h2 class="card-title font-bold">Suggested places</h2>
+        </div>
         {#each suggestions as suggestion}
             <button
-                class="card card-body items-center text-center date-div accept-suggestion"
+                class="card card-body items-center text-center date-div accept-suggestion rounded accept-suggestion"
                 on:click|preventDefault={() => addEvent(suggestion)}
             >
                 <h2 class="card-title font-bold">{suggestion.placeName}</h2>
             </button>
         {/each}
+        {#if suggestions.length == 0}
+            <div class="card card-body items-center text-center date-div">
+                <h2 class="card-title font-bold">No more suggestions</h2>
+            </div>
+        {/if}
 
         <div>
             <input
@@ -375,7 +383,7 @@
             >
         </div>
     </div>
-    <div class="card w-96 bg-primary bg-info text-base-200 h-full">
+    <div class="card w-96 bg-base-content text-base-200 h-full">
         <div class="card-body">
             <h2 class="card-title">About {placeTo}</h2>
             <p class="text-wrap break-all">{chatDetail}</p>
